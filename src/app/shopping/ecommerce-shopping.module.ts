23e6db09fe7b8b73bbffe48e154edcb611c90bce
krashishsoni <kr.ashishsoni@gmail.com>
2019-07-03 16:36:49 +0800

    2 files modified, 3 added

--- src/app/shopping/ecommerce-shopping.module.ts
+++ src/app/shopping/ecommerce-shopping.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ShoppingCartModule } from './cart/shopping-cart.module';
 import { ECartService } from './services/e-cart.service';
 import { ProductPageComponent } from './product-page/product-page.component';
+import { ProductcardComponent } from './productcard/productcard.component';
 @NgModule({
   imports: [
     CommonModule,
     ShoppingCartModule
   ],
   exports: [ShoppingCartModule,ProductPageComponent],
-  declarations: [ProductPageComponent],
+  declarations: [ProductPageComponent, ProductcardComponent],
   providers: [ECartService]
 })
 export class EcommerceShoppingModule { }
